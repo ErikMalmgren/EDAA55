7969a12b73094c0e83c411a8c45a4fffa25f2875
Erik Malmgren <e.malmis@gmail.com>
2020-11-11 09:50:39 +0100

    Färger

--- Lab01-introduktion/src/SimpleWindowExample.java
+++ Lab01-introduktion/src/SimpleWindowExample.java
@@ -1,13 +1,17 @@
 import se.lth.cs.pt.window.SimpleWindow;
+import java.awt.Color;
 
 public class SimpleWindowExample {
 	public static void main(String[] args) {
 		SimpleWindow w = new SimpleWindow(500, 500, "Drawing Window");
+		Color nyFärg = new Color(252, 12, 96);
 		w.moveTo(100, 100);
 		w.setLineWidth(5);
+		w.setLineColor(nyFärg);
 		w.lineTo(150, 100);
 		w.lineTo(150, 150);
+		w.setLineColor(Color.PINK);
 		w.lineTo(100, 100);
-		w.lineTo(200, 200);
+		w.lineTo(50, 200);
 	}
 }
