8aae8e089e648d8d6a6d24e0fe0e871391e1433c
Erik Malmgren <e.malmis@gmail.com>
2020-11-12 11:00:07 +0100

    QoL ändring

--- Lab02-anv-square/src/DrawThreeSquares.java
+++ Lab02-anv-square/src/DrawThreeSquares.java
@@ -5,7 +5,7 @@ public class DrawThreeSquares {
 	public static void main(String[] args) {
 		SimpleWindow w = new SimpleWindow(600, 600, "DrawSquare");
 		Square sq = new Square(250, 250, 100);
-		//Square kv = null;
+		//Square sq = null;
 		sq.draw(null);
 		sq.move(50,-50);
 		sq.draw(w);
