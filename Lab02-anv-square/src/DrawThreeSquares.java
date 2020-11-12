455d6be65cd1b0152b0de2eb935103cea5d3ebe6
Erik Malmgren <e.malmis@gmail.com>
2020-11-12 11:27:50 +0100

    Redovisning labb 1 och 2

--- Lab02-anv-square/src/DrawThreeSquares.java
+++ Lab02-anv-square/src/DrawThreeSquares.java
@@ -6,7 +6,7 @@ public class DrawThreeSquares {
 		SimpleWindow w = new SimpleWindow(600, 600, "DrawSquare");
 		Square sq = new Square(250, 250, 100);
 		//Square sq = null;
-		sq.draw(null);
+		sq.draw(w);
 		sq.move(50,-50);
 		sq.draw(w);
 		sq.move(-75,25);
