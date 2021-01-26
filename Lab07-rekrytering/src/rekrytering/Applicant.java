cf59f28da9dcdbd88fcf0b72ca3574f58bd76566
Erik Malmgren <e.malmis@gmail.com>
2021-01-26 14:03:44 +0100

    Lab 7 klar

--- Lab07-rekrytering/src/rekrytering/Applicant.java
+++ Lab07-rekrytering/src/rekrytering/Applicant.java
@@ -1,7 +1,7 @@
 package rekrytering;
 
 public class Applicant implements Comparable<Applicant> {
-	//Varje sökande har ett namn och ett antal betyg
+	// Varje sökande har ett namn och ett antal betyg
 	private String name;
 	private int[] grades;
 
@@ -24,27 +24,55 @@ public class Applicant implements Comparable<Applicant> {
 			if (g[i].equals("U")) {
 				// Om underkänd så räknar vi det som en nolla
 				grades[i] = 0;
-			} else {
+			}
+			else if (g[i].equals("A")) {
+				grades[i] = 5;
+			}
+			else if (g[i].equals("B")) {
+				grades[i] = 4;
+			}
+			else if (g[i].equals("C")) {
+				grades[i] = 3;
+			}
+			else if (g[i].equals("D")) {
+				grades[i] = 2;
+			}
+			else if (g[i].equals("E")) {
+				grades[i] = 1;
+			}
+			else if (g[i].equals("F")) {
+				grades[i] = 0;
+			}
+			
+			else {
 				grades[i] = Integer.parseInt(g[i]);
 			}
+
 		}
 	}
 
 	public double getAvgGrade() {
-		return 0; 
+		double total = 0;
+		for (int i = 0; i < grades.length; i++) {
+			total += grades[i];
+		}
+		return total / grades.length;
+	}
+
+	public String toString() {
+		String gradesAsString = "[";
+		for (int i = 0; i < grades.length; i++) {
+			gradesAsString += grades[i] + ", ";
+		}
+		gradesAsString += grades[grades.length - 1] + "]";
+		return name + gradesAsString + "(avg: " + getAvgGrade() + ")";
 	}
 
 	/*
-	  Implementera denna när labbeskrivningen kräver det 
-	  public String toString() {
-	      //Fyll i kod här 
-	  }
-	 */
-
-	/*
 	 * Metod för att jämföra detta Applicant-objekt med ett annat och få ut vilket
-	 * som är störst. Retunerar något > 0 om detta objektet är störst. Returnerar något < 0 om other är störst och returnerar 0 om objekten är lika.
-	 * Används av javas inbyggda sorteringsmetoder
+	 * som är störst. Retunerar något > 0 om detta objektet är störst. Returnerar
+	 * något < 0 om other är störst och returnerar 0 om objekten är lika. Används av
+	 * javas inbyggda sorteringsmetoder
 	 */
 	public int compareTo(Applicant other) {
 		// Om exakt samma objekt
--- Lab07-rekrytering/src/rekrytering/FindBestCandidates.java
+++ Lab07-rekrytering/src/rekrytering/FindBestCandidates.java
@@ -1,22 +1,51 @@
 package rekrytering;
 
+
+import java.util.Arrays;
+
 public class FindBestCandidates {
-	private static final double MIN_AVG_GRADE = 4.0;
+	private static final double MIN_AVG_GRADE = 0.0;
 
 	public static void main(String[] args) {
 		// Läs från fil (Börja med "applications_small.txt), spara resultatet i en
 		// vektor
-
+		Applicant[] applicants = FileReader.readFromFile("applications_all.txt", 231);
 		// Skicka in Applicant-vektorn (som du fick i föregående steg) till metoden
 		// findBestCandidiates (nedan)
 		// Spara resultatet i en vektor
-
+		
 		// Printa resultatet från findBestCandidates
+		Applicant[] bestCandidates = findBestCandidates(applicants);
+		Arrays.sort(bestCandidates);
+		System.out.println("Best Candidates:");
+		PrintApplicants(bestCandidates);
 	}
 
 	public static Applicant[] findBestCandidates(Applicant[] applicants) {
 		// Hitta alla kandidater som har medelbetyg över MIN_AVG_GRADE
 		// Lagra alla dessa kandidater i en vektor, returnera vektorn
-		return null; //Byt ut denna rad mot hela din egen lösning
+		
+		int goodCandidateCount = 0;
+		Applicant[] goodCandidates = new Applicant[applicants.length];
+		
+		for (int i = 0; i < applicants.length; i++ ) {
+			if (applicants[i].getAvgGrade() > MIN_AVG_GRADE) {
+				goodCandidates[goodCandidateCount] = applicants[i];
+				goodCandidateCount++;
+				
+			}
+		}
+		
+		Applicant[] returnCandidates = new Applicant[goodCandidateCount];
+		for (int i = 0; i < goodCandidateCount; i++) {
+			returnCandidates[i] = goodCandidates[i];
+		}
+		return returnCandidates;
+	}
+	
+	private static void PrintApplicants (Applicant[] applicants) {
+		for (int i= 0; i < applicants.length; i++) {
+			System.out.println(applicants[i]);
+		}
 	}
 }
