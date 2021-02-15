8679b5049f079508593f1b6bd832575947579a72
Erik Malmgren <e.malmis@gmail.com>
2021-02-15 14:30:04 +0100

    Lab 8 klar

--- Lab08-memory/src/memory/MemoryBoard.java
+++ Lab08-memory/src/memory/MemoryBoard.java
@@ -1,50 +1,86 @@
 package memory;
-
+import java.util.Random;
 
 public class MemoryBoard {
-
+	int size;
+	MemoryCardImage[][] deck;
+	boolean[][] isTurned;
+	
+	
+	
 	/** Skapar ett memorybräde med size * size kort. backFileName är filnamnet 
 	    för filen med baksidesbilden. Vektorn frontFileNames innehåller filnamnen 
 	    för frontbilderna. */
 	public MemoryBoard(int size, String backFileName, String[] frontFileNames) {
-
+		this.size = size;
+		this.deck = new MemoryCardImage[size][size];
+		this.isTurned = new boolean[size][size];
+		this.createCards(backFileName, frontFileNames);
+		
 	}
 
 	/* Skapar size * size / 2 st memorykortbilder.
 	   Placerar ut varje kort på två slumpmässiga ställen på spelplanen. */
 	private void createCards(String backFileName, String[] frontFileNames) {
-
+		if (size % 2!= 0 || size > frontFileNames.length /2) {
+			System.out.println("Error! Size must be a multiple of 2 and no more than "+ frontFileNames.length /2);
+		}
+		
+		for ( int i = 0; i < ((size*size)/2); i++) {
+			MemoryCardImage card = new MemoryCardImage(frontFileNames[i], backFileName);
+			placeCard(card);
+			placeCard(card);
+		}
+	}
+	
+	private void placeCard(MemoryCardImage card) {
+		Random rand = new Random();
+		int r = rand.nextInt(size), c = rand.nextInt(size);
+		if (this.deck[r][c] == null) {
+			this.deck[r][c] = card;
+		} else {
+			placeCard(card);
+		}
 	}
 
 	/** Tar reda på brädets storlek. */
 	public int getSize() {
-		return 0;
+		return this.size;
 	}
 	
 	/** Hämtar den tvåsidiga bilden av kortet på rad r, kolonn c.
 	    Raderna och kolonnerna numreras från 0 och uppåt. */
 	public MemoryCardImage getCard(int r, int c) {
-		return null;
+		return this.deck[r][c];
 	}
 
 	/** Vänder kortet på rad r, kolonn c. */
 	public void turnCard(int r, int c) {
-
+		this.isTurned[r][c] = !this.isTurned[r][c];
 	}
 	
 	/** Returnerar true om kortet r, c har framsidan upp. */
 	public boolean frontUp(int r, int c) {
-		return false;
+		return this.isTurned[r][c];
 	}
 	
 	/** Returnerar true om det är samma kort på rad r1, kolonn c2 som på rad r2, 
 	    kolonn c2. */
 	public boolean same(int r1, int c1, int r2, int c2) {
-		return false;
+		return this.deck[r1][c1].equals(this.deck[r2][c2]);
 	}
 
 	/** Returnerar true om alla kort har framsidan upp. */
 	public boolean hasWon() {
-		return false;
+		for (int r = 0; r < size; r++) {
+			for(int c = 0; c < size; c++) {
+				if(!this.isTurned[r][c]) {
+					return false;
+				}
+			}
+		}
+		
+		
+		return true;
 	}	
 }
