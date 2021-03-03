b0559c339eaa0c7d049fddac7ae3b52c3059ba80
Erik Malmgren <e.malmis@gmail.com>
2021-03-03 10:38:18 +0100

    Projektet forts

--- Bank projekt/src/Bank.java
+++ Bank projekt/src/Bank.java
@@ -1,31 +1,44 @@
+import java.util.ArrayList;
+
 public class Bank {
-	private String name;
-	private long idNr;
-	private int customerNr;
-	private static int nextCustomerNr = 100;
-	
-	// Skapar ett konto med "name" och "idNr" samt tilldelas ett unikt kundnummer
+	private ArrayList<BankAccount> accounts;
 
-	public Customer(String name, long idNr) {
-        this.name = name;
-        this.idNr = idNr;
-        this.customerNr = nextCustomerNr;
-        nextCustomerNr++;
-    }
-
-	public String getName() {
-		return this.name;
+	/** Skapar en ny bank utan konton. */
+	public Bank() {
+		this.accounts = new ArrayList<BankAccount>();
 	}
 
-	public long getIdNr() {
-		return this.idNr;
+	/**
+	 * Öppna ett nytt konto i banken. Om det redan finns en kontoinnehavare med de
+	 * givna uppgifterna ska inte en ny Customer skapas, utan istället den
+	 * befintliga användas. Det nya kontonumret returneras.
+	 */
+	public int addAccount(String holderName, long idNr) {
+		Customer holder = findHolder(idNr);
+		if (holder == null) {
+			holder = new Customer(holderName, idNr);
+		}
+		if (!holder.getName().equals(holderName)) {
+			return -1;  // Får bara finnas en kund med id nr och namn
+			}
+
+		BankAccount account = new BankAccount(holder);
+		accounts.add(account);
+		return account.getAccountNumber();
+		
 	}
 
-	public int getCustomerNr() {
-		return this.customerNr;
-	}
+	/**
+	 * Returnerar den kontoinnehavaren som har det givna id-numret, eller null om
+	 * ingen sådan finns.
+	 */
 
-	public String toString() {
-		return this.name + ", id " + this.idNr + ", kundnr " + this.getCustomerNr();
+	public Customer findHolder(long idNr) {
+		for (BankAccount acc : accounts) {
+			if (acc.getHolder().getIdNr() == idNr) {
+				return acc.getHolder();
+			}
+		}
+		return null;
 	}
 }
--- Bank projekt/src/Customer.java
+++ Bank projekt/src/Customer.java
@@ -1,4 +1,41 @@
 
 public class Customer {
 
+	private String name;
+	private long idNr;
+	private int customerNr;
+	private static int nextCustomerNr = 100;
+
+	/**
+	 * Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
+	 * Kunden tilldelas också ett unikt kundnummer.
+	 */
+
+	public Customer(String name, long idNr) {
+		this.name = name;
+		this.idNr = idNr;
+		this.customerNr = nextCustomerNr;
+		nextCustomerNr++;
+	}
+
+	/** Tar reda på kundens namn. */
+
+	public String getName() {
+		return this.name;
+	}
+
+	/** Tar reda på kundens personnummer. */
+	public long getIdNr() {
+		return this.idNr;
+	}
+
+	/** Tar reda på kundens kundnummer. */
+	public int getCustomerNr() {
+		return this.customerNr;
+	}
+
+	/** Returnerar en strängbeskrivning av kunden. */
+	public String toString() {
+		return this.name + ", id " + this.idNr + ", kundnr " + this.getCustomerNr();
+	}
 }
