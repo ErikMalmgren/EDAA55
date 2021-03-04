b502c3746e8677a21a217f2b6dbe25e08b335fd5
Erik Malmgren <e.malmis@gmail.com>
2021-03-04 11:46:41 +0100

    Fram till case 5 på BankApplication

--- Bank projekt/src/BankApplication.java
+++ Bank projekt/src/BankApplication.java
@@ -1,4 +1,226 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class BankApplication {
+	private Bank bank;
+	private Scanner scan;
+	private Random rand;
+
+	public static void main(String[] args) {
+		BankApplication app = new BankApplication();
+	}
+
+	private void initialize() {
+		this.bank = new Bank();
+		this.scan = new Scanner(System.in);
+		this.rand = new Random();
+
+		this.run();
+	}
+
+	private void run() {
+		int selection = this.handleMenu();
+
+		switch (selection) {
+		case 1: // Hitta konto utifrån innehavare
+			this.findAccountForHolder();
+			break;
+		case 2: // Hitta konto från namn
+			this.findAccountByPartOfName();
+			break;
+		case 3: //Sätta in
+			this.deposit();
+			break;
+		case 4: //Ta ut
+			this.withdraw();
+			break;
+		case 5: //Överföring
+			
+		}
+
+	}
+
+	// Menyn
+
+	private int handleMenu() {
+		String[] menuItems = new String[] { "Hitta konto utifrån innehavare",
+				"Sök kontoinnehavare utifrån (del av) namn", "Sätt in", "Ta ut", "Överföring", "Skapa konto",
+				"Ta bort konto", "Skriv ut konton", "Avsluta" };
+
+		System.out.print("\n");
+		for (int i = 0; i < 24; i++) {
+			System.out.print(" - ");
+		}
+		System.out.print("\n");
+
+		for (int i = 0; 1 < menuItems.length; i++) {
+			this._print((i + 1) + ": " + menuItems[i]);
+		}
+
+		int selection = this._waitForInputI("val");
+
+		// Reprinta menyn om valet är utanför intervallet
+		if (selection > menuItems.length + 1 || selection < 1) {
+			return this.handleMenu();
+		}
+		return selection;
+	}
+
+	
+
+	// Case 1: Hitta konto utifrån innehavare
+
+	private void findAccountForHolder () {
+		var id = this._waitForInputL("Sök kundnummer");
+		var found = this.bank.findAccountsForHolder(id);
+		var length = found.size();
+		
+		if (length > 0) {
+			for (int i = 0; i < length; i++) {
+				var acc = found.get(i);
+				this._print(acc);
+			}
+		} else {
+			this._print("Inga resultat hittades");
+		}
+	}
+	
+	// Case 2: Hitta konto från namn
+	
+	private void findAccountByPartOfName() {
+		String searchTerm = this._waitForInput("Sök: ");
+		var searchResults = bank.findByPartOfName(searchTerm);
+		
+		if (searchResults.size() == 0) {
+			this._print("inga resultat hottades");
+		} else {
+			searchResults.forEach(s -> this._print(s));
+		}
+	}
+	// case 3: Sätta in
+	
+	private void deposit() {
+		int accountNbr = this._waitForInputI("konto");
+		var account = this.bank.findByNumber(accountNbr);
+		
+		if (account == null) {
+			this._print("kontot finns inte");
+			return;
+		}
+		
+		long amount = this._getAmount(account.getAmount(), false);
+		
+		account.deposit(amount);
+		this._print(account);
+	}
+	
+	//case 4: Ta ut
+	
+	private void withdraw() {
+		int accountNbr = this._waitForInputI("konto");
+		var account = this.bank.findByNumber(accountNbr);
+		
+		if ( account == null) {
+			this._print("Kontot finns inte");
+			return;
+		}
+		long amount = this._getAmount(account.getAmount(), true);
+
+        account.withdraw(amount);
+
+        this._print(account);
+	}
+	
+	
+	//case 5: Överföring
+	
+	private void transfer() {
+		int fromAccountNbr = this._waitForInputI("från");
+        int toAccountNbr = this._waitForInputI("till");
+        
+        var fromAccount = this.bank.findByNumber(fromAccountNbr);
+
+        if (fromAccount == null) {
+            this._print("Kontot finns inte");
+            return;
+        }
+        long amount = this._getAmount(fromAccount.getAmount(), true);
+
+        fromAccount.withdraw(amount);
+        
+        var toAccount = this.bank.findByNumber(toAccountNbr);
+
+        if (toAccount == null) {
+            this._print("Kontot finns inte");
+            return;
+        }
+
+        toAccount.deposit(amount);
+
+        this._print(fromAccount);
+        this._print(toAccount);
+	}
+	
+	
+	
+	// Hjälpmetoder
+	
+	private void _print(Object s) {
+		System.out.println(s);
+	}
+	
+	private String _waitForInput(String label) {
+		if (label !="") {
+			System.out.print(label + ": ");
+		}
+		var input = scan.nextLine();
+		return input;
+	}
+
+	private int _waitForInputI(String label) {
+		if (label != "") {
+			System.out.print(label + ": ");
+		}
+		int input;
+
+		try {
+			input = scan.nextInt();
+		} catch (Exception e) {
+			scan.nextLine();
+			return this._waitForInputI(label);
+		}
+		scan.nextLine();
+		return input;
+
+	}
+
+	private long _waitForInputL(String label) {
+		if (label != "") {
+			System.out.print(label + ": ");
+		}
+
+		long input;
+
+		try {
+			input = scan.nextLong();
+		} catch (Exception e) {
+			input = -1;
+		}
+		scan.nextLine();
+		return input;
+	}
+	
+	private long _getAmount(double currentAmount, Boolean checkAgainstCurrent) {
+		long amount = this._waitForInputL("Belopp");
+		if (amount < 0) {
+			this._print("Beloppet kan ej vara mindre än 0");
+            return this._getAmount(currentAmount, checkAgainstCurrent);
+		}
+		if (checkAgainstCurrent && amount > currentAmount) {
+            this._print("Finns inte tillräckligt med täckning.");
+            return this._getAmount(currentAmount, checkAgainstCurrent);
+		}
+		return amount;
+	}
 
 }
